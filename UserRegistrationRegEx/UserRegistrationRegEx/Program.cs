8c7d97e0ea8e2a292d58ec2034c41d5770fa69d1
hem_b <hemanthb93@gmail.com>
2022-06-08 19:43:15 +0530

    [Hemanth]:Validates Last name with regex pattern

--- UserRegistrationRegEx/UserRegistrationRegEx/Program.cs
+++ UserRegistrationRegEx/UserRegistrationRegEx/Program.cs
@@ -2,3 +2,7 @@
 Console.WriteLine("Enter the first name of user :");
 string fName = Console.ReadLine();
 registrationCheck.FirstNameValidCheck(fName);
+Console.WriteLine();
+Console.WriteLine("Enter the Last name of user :");
+string lName = Console.ReadLine();
+registrationCheck.LastNameValidCheck(lName);
--- UserRegistrationRegEx/UserRegistrationRegEx/UserRegistrationCheck.cs
+++ UserRegistrationRegEx/UserRegistrationRegEx/UserRegistrationCheck.cs
@@ -10,6 +10,7 @@ namespace UserRegistrationRegEx
     public class UserRegistrationCheck
     {
         string firstName = "^[A-Z]{1}[a-z]{2,}";
+        string lastName  = "^[A-Z]{1}[a-z]{2,}";
         public void FirstNameValidCheck(string fname)
         {
             if (Regex.Match(fname, firstName).Success)
@@ -18,6 +19,15 @@ namespace UserRegistrationRegEx
                 return;
             }
             Console.WriteLine("First name isn't Valid...Re-Enter!");
+        }
+        public void LastNameValidCheck(string lname)
+        {
+            if (Regex.Match(lname, lastName).Success)
+            {
+                Console.WriteLine("Last Name Entered is Valid");
+                return;
+            }
+            Console.WriteLine("Last name isn't Valid...Re-Enter!");
         }
     }
 }
