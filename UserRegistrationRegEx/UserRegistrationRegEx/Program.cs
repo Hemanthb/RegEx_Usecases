e476432aeb05912545e95c5515c7d2709f0e279c
hem_b <hemanthb93@gmail.com>
2022-06-08 17:16:55 +0530

    [Hemanth]:Checks the Firstname with the regex Pattern

---
+++ UserRegistrationRegEx/UserRegistrationRegEx/Program.cs
@@ -0,0 +1,4 @@
+﻿UserRegistrationRegEx.UserRegistrationCheck registrationCheck = new UserRegistrationRegEx.UserRegistrationCheck();
+Console.WriteLine("Enter the first name of user :");
+string fName = Console.ReadLine();
+registrationCheck.FirstNameValidCheck(fName);
---
+++ UserRegistrationRegEx/UserRegistrationRegEx/UserRegistrationCheck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationRegEx
+{
+    public class UserRegistrationCheck
+    {
+        string firstName = "^[A-Z]{1}[a-z]{2,}";
+        public void FirstNameValidCheck(string fname)
+        {
+            if (Regex.Match(fname, firstName).Success)
+            {
+                Console.WriteLine("First Name Entered is Valid");
+                return;
+            }
+            Console.WriteLine("First name isn't Valid...Re-Enter!");
+        }
+    }
+}
